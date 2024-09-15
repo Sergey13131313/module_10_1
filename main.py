153cb0b755dfbb620996d5b8907ea549444f0fde
Sergey13131313 <110328066+Sergey13131313@users.noreply.github.com>
2024-09-15 14:51:22 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,30 @@
-import time
 from threading import Thread
+import time
+
+ENEMY = 100
 
 
-def wite_words(word_count, file_name):
-    for i in range(word_count):
-        print(f'Какое-то слово № {i}')
-        time.sleep(0.1)
+class Knight(Thread):
 
-    print(f'Завершилась запись в файл {file_name}')
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
 
-
-start_time = time.time()
-
-wite_words(10, 'example1.txt')
-wite_words(30, 'example2.txt')
-wite_words(200, 'example3.txt')
-wite_words(100, 'example4.txt')
-
-end_time = time.time()
-print(f'Работа потоков {round(end_time - start_time, 2)}')
-
-start_time = time.time()
-
-thr_1 = Thread(target=wite_words, args=(10, 'example5.txt'))
-thr_2 = Thread(target=wite_words, args=(30, 'example6.txt'))
-thr_3 = Thread(target=wite_words, args=(200, 'example7.txt'))
-thr_4 = Thread(target=wite_words, args=(100, 'example8.txt'))
-
-thr_1.start()
-thr_2.start()
-thr_3.start()
-thr_4.start()
-
-thr_1.join()
-thr_2.join()
-thr_3.join()
-thr_4.join()
+    def run(self):
+        count_day = 0
+        for i in range(ENEMY, 0, -self.power):
+            time.sleep(1)
+            count_day += 1
+            print(f'{self.name} сражается {count_day} , осталось {i} воинов.')
+        print(f'{self.name} одержал победу спустя {count_day} дней(дня)!')
 
 
-end_time = time.time()
-print(f'Работа потоков {round(end_time - start_time, 2)}')
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
