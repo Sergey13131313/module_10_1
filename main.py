874b1ea52e2786883c6d4d801c20764bc87109f8
Sergey13131313 <110328066+Sergey13131313@users.noreply.github.com>
2024-10-01 12:35:41 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-from threading import Thread
 import time
-
-ENEMY = 100
-
-
-class Knight(Thread):
-
-    def __init__(self, name, power):
-        super().__init__()
-        self.name = name
-        self.power = power
-
-    def run(self):
-        count_day = 0
-        for i in range(ENEMY, 0, -self.power):
-            time.sleep(1)
-            count_day += 1
-            print(f'{self.name} сражается {count_day} , осталось {i} воинов.')
-        print(f'{self.name} одержал победу спустя {count_day} дней(дня)!')
+from threading import Thread
 
 
-first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
+def word(n) -> str:
+    return f'Какое-то слово № {n}\n'
 
-first_knight.start()
-second_knight.start()
 
-first_knight.join()
-second_knight.join()
+def wite_words(word_count, file_name):
+    with open(file_name, mode="w") as f:
+        for i in range(1, word_count + 1):
+            w = word(i)
+            f.write(w)
+            time.sleep(0.1)
+    print(f'Завершилась запись в файл {file_name}')
+
+
+time_start = time.time()
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+time_stop = time.time()
+
+print(f'Работа потоков {time.strftime("%H:%M:%S", time.gmtime(time_stop - time_start))}')
+
+tr_list = []
+count_words = [10,30,200,100]
+
+time_start = time.time()
+for i in range(5, 9):
+    filename = f'example{i}.txt'
+    tr = Thread(target=wite_words, args=(count_words[i - 5], filename))
+    tr_list.append(tr)
+
+[tr.start() for tr in tr_list]
+[tr.join() for tr in tr_list]
+time_stop = time.time()
+
+print(f'Работа потоков {time.strftime("%H:%M:%S", time.gmtime(time_stop - time_start))}')
